cbb0ce8a29b834826e4864654905b6b656c20a8c
Vadim Hmys <vadimhmyz@gmail.com>
2024-03-06 22:53:31 +0300

    feat: add filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
 import React, { useState } from 'react';
 import './App.css';
-import { TodoList } from './TodoList';
+import { TaskType, TodoList } from './TodoList';
+
+export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
-  const [tasks, setTasks] = useState([
+  let [tasks, setTasks] = useState<Array<TaskType>>([
     { id: 1, title: 'CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
     { id: 3, title: 'REACT', isDone: false },
     { id: 4, title: 'Redux', isDone: false }
   ]);
 
+  let [filter, setFilter] = useState<FilterValuesType>("all");
+
   function removeTask(id: number) {
     setTasks(tasks.filter(t => t.id !== id));
+  }
+
+  function changeFilter(value: FilterValuesType) {
+    setFilter(value);
+  }
+
+  let tasksForTodoList = tasks;
+  if (filter === "completed") {
+    tasksForTodoList = tasks.filter(t => t.isDone === true);
+  }
+  if (filter === "active") {
+    tasksForTodoList = tasks.filter(t => t.isDone === false);
   }
   
   return (
     <div className="App">
       <TodoList 
         title="What to learn" 
-        tasks={tasks}
-        removeTask={removeTask}/>
+        tasks={tasksForTodoList}
+        removeTask={removeTask}
+        changeFilter={changeFilter}
+        />
     </div>
   );
 }
