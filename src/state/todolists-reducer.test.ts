172ed99ec3c26dcbc54611626ec484f3c3da6f4b
Vadim Hmys <vadimhmyz@gmail.com>
2024-04-03 22:50:43 +0300

    refactor: define actions types

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -48,7 +48,7 @@ test('correct todolist should change its name', () => {
  ];
 
  const action = {
-  type: 'CHANGE-TODOLIST-TITLE',
+  type: 'CHANGE-TODOLIST-TITLE' as const,
   id: todolistId2,
   title: newTodolistTitle
  };
@@ -71,7 +71,7 @@ test('correct filter of todolist should be changed', () => {
   ];
 
   const action = {
-    type: 'CHANGE-TODOLIST-FILTER',
+    type: 'CHANGE-TODOLIST-FILTER' as const,
     id: todolistId2,
     filter: newFilter
   };
